b415d9279ece0d9f93916132e996b4fd862f1f84
David-gitbash <mtzmolinad35@gmail.com>
2023-03-26 19:30:15 -0600

    Parte David completada

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+/*FUNCIONES FOOTER*/
+
+function redireccionFacebook() {
+  location.href = "https://www.facebook.com/escueladeingenieriaycienciaspuebla";
+}
+
+function redireccionIG() {
+  location.href = "https://www.instagram.com/ingenieriaspue";
+}
+
+function redireccionLogo() {
+  location.href = "index.html";
+}
