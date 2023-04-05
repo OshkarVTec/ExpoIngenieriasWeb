c42714cfdc91d98613d52fcf526378ac20af697c
OshkarVTec <a01275287@tec.mx>
2023-04-05 11:02:28 -0600

    Added php to ver_proyectos

---
+++ ver_proyectos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Ver proyectos</title>
+   <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+   <header class="header">
+      <div class="logo">
+        <img src="IMG/logo-expo.png" alt="Logo de la pagina">
+      </div>
+      <a href="#" class="btn"><button>Log In</button></a>
+   </header>
+   <?php
+      include 'database.php';
+      $pdo = Database::connect();
+      $sql = 'SELECT * FROM r_categorias ORDER BY id_categoria';
+      foreach ($pdo->query($sql) as $row) {
+         echo '<h1 class="label">'.$row['nombre'].'</h1>';
+         echo '<div class = "container">';
+         echo '<table class="general">';
+         $sql = 'SELECT * FROM r_proyectos WHERE id_categoria ='.$row['id_categoria'];
+         foreach ($pdo->query($sql) as $row) {
+            echo '<tr>';
+            echo '<td>';
+            echo '<button class="btn" href="read.php?id='.$row['idauto'].'">'. $row['nombre'] .'</a>';
+            echo '</td>';
+            echo '</tr>';
+         }
+         echo '</table>';
+         echo '</div>';
+      }
+      Database::disconnect();
+   ?>
+</body>
+</html>
