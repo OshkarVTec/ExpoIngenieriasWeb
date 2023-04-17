8491432ec65df1ba2df31a9c1fd75bed27ac0ef0
Erwin <a01734881@tec.mx>
2023-04-17 16:19:23 -0600

    Proyecto formato

--- proyecto.php
+++ proyecto.php
@@ -10,24 +10,34 @@
 <body>
    <header class="header">
       <div class="logo">
+        <a href="informativa_presentacion.html">
         <img src="IMG/logo-expo.png" alt="Logo de la pagina">
+        </a>
       </div>
-      <a href="#" class="btn"><button>Log In</button></a>
+      <a href="login.php" class="btn"><button>Log In</button></a>
     </header>
    
     <div class="container">
         <div class="firstrow">
             <h1 class="title">Nombre del Proyecto</h1>
             <p class ="status">Aprobado/Rechazado</p>
+            <p class ="status">UF10034</p>
             <a href="#" class="btnP"><button>Editar</button></a>
         </div>
         <hr color="#1687A7">
         <div class="secondrow">
+            <div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis interdum felis. Vivamus porta nibh a eros lobortis suscipit. Sed in sollicitudin ex. Proin mattis aliquet tempor. Vivamus aliquet magna vel enim tincidunt, vitae iaculis nisi bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis interdum felis. Vivamus porta nibh a eros lobortis suscipit. Sed in sollicitudin ex. Proin mattis aliquet tempor. Vivamus aliquet magna vel enim tincidunt, vitae iaculis nisi bibendum.
 
                 s aliquet magna vel enim tincidunt, vitae iaculis nisi bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis interdum felis. Vivamus porta nibh a eros lobortis suscipit. Sed in sollicitudin ex. Proin mattis aliquet tempor. Vivamus aliquet magna vel enim tincidunt, vitae iaculis nisi bibendum.
             </p>
-            <img src="IMG/testimage.jpg" alt="testimage" class="img">
+            <br>
+            <h1>Video del Proyecto</h1>
+            <iframe src="https://drive.google.com/file/d/0BwXl-_bCkYu-Ym5zakZhaGZCRlU/preview" width="560" height="315"></iframe>
+            </div>  
+            <img src="https://drive.google.com/uc?export=view&id=1j38GdZ6kT5A71vulyNBWiST9jN_bXpzH" alt="testimage" class="img">
+        </div>
+        <div class="videorow">
         </div>
         <hr color="#1687A7">
         <div class="thirdrow">
