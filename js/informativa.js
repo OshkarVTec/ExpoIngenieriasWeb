e318d6776dfd84a592615131926497f74732d88d
David-Mtz-M <mtzmolinad35@gmail.com>
2023-04-23 15:04:45 -0700

    slider automatico + manual

--- js/informativa.js
+++ js/informativa.js
@@ -1,14 +1,34 @@
 let slideIndex = 1;
+let slideTimer = null;
+
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
+  if (slideTimer) {
+    clearTimeout(slideTimer);
+    slideTimer = null;
+  }
   showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+  if (slideTimer) {
+    clearTimeout(slideTimer);
+    slideTimer = null;
+  }
   showSlides((slideIndex = n));
+}
+
+function startSlideTimer() {
+  if (slideTimer) {
+    clearTimeout(slideTimer);
+  }
+  slideTimer = setTimeout(function () {
+    plusSlides(1);
+    startSlideTimer();
+  }, 5000); // Change this value to set the time between automatic slide changes
 }
 
 function showSlides(n) {
@@ -29,4 +49,7 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+
+  // Start the slide timer
+  startSlideTimer();
 }
