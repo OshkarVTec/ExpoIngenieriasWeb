1be6cf7f781755de898c50a55b279ebea2e3c73b
OshkarVTec <a01275287@tec.mx>
2023-05-03 12:52:57 -0600

    Validación de inicio de sesión

--- aprobar_proyectos.php
+++ aprobar_proyectos.php
@@ -1,33 +1,36 @@
 <?php
-   session_start();
-   include 'database.php';
-   session_start();
-   // if($_SESSION['id_docente'] != null) $id_docente = $_SESSION['id_docente'];
-   // else header("Location:informativa.php");
-   if ( !empty($_POST)) {
-      $id_proyecto = $_POST['id'];
-		if (isset($_POST['aprobar'])) {
-         $pdo = Database::connect();
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "UPDATE r_proyectos 
+session_start();
+include 'database.php';
+session_start();
+if ($_SESSION['id_docente'] != null)
+   $id_juez = $_SESSION['id_docente'];
+else
+   header("Location:informativa.php");
+if (!empty($_POST)) {
+   $id_proyecto = $_POST['id'];
+   if (isset($_POST['aprobar'])) {
+      $pdo = Database::connect();
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "UPDATE r_proyectos 
                   SET estatus = 1
                   WHERE id_proyecto = ?";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($id_proyecto));
-			Database::disconnect();
-      } else if (isset($_POST['rechazar'])) {
-         $pdo = Database::connect();
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "DELETE FROM r_proyectos 
+      $q = $pdo->prepare($sql);
+      $q->execute(array($id_proyecto));
+      Database::disconnect();
+   } else if (isset($_POST['rechazar'])) {
+      $pdo = Database::connect();
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "DELETE FROM r_proyectos 
                   WHERE id_proyecto = ?";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($id_proyecto));
-			Database::disconnect();
-      }
+      $q = $pdo->prepare($sql);
+      $q->execute(array($id_proyecto));
+      Database::disconnect();
    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -35,72 +38,75 @@
    <title>Aprobar proyectos</title>
    <link rel="stylesheet" href="CSS/style.css">
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-   <script> 
-    $(function(){
-      $("#header").load(<?php 
-        if($_SESSION['estudiante'] != null){
+   <script>
+      $(function () {
+         $("#header").load(<?php
+         if ($_SESSION['estudiante'] != null) {
             echo '"header_estudiante.php"';
-        } else if($_SESSION['admin'] != null){
+         } else if ($_SESSION['admin'] != null) {
             echo '"header_admin.php"';
-        } else if($_SESSION['juez'] != null){
+         } else if ($_SESSION['juez'] != null) {
             echo '"header_juez.php"';
-        } else if($_SESSION['docente'] != null){
+         } else if ($_SESSION['docente'] != null) {
             echo '"header_docente.php"';
-        }
-        ?>); 
-    });
-    </script> 
+         }
+         ?>);
+      });
+   </script>
 </head>
+
 <body>
    <div id="header"></div>
    <h1 class="label">Por aprobar</h1>
-   <p class="label">En caso de rechazar un proyecto, este se eliminará. Para pedir cambios al proyecto antes de aprobarlos por favor contacte a los estudiantes directamente.</p>
-   <div class = "container">
-      <table class="general">  
-   <?php
-      $pdo = Database::connect();
-      $sql = 'SELECT *  FROM r_proyectos WHERE id_docente = ? AND estatus = 0';
-      $q = $pdo->prepare($sql);
-      $q->execute(array($id_docente));
-      $proyectos = $q->fetchAll(PDO::FETCH_ASSOC);
-      foreach ($proyectos as $row) {
-         echo '<tr>';
-         echo '<td>';
-         echo '<a class="link" href="proyecto.php?id='.$row['id_proyecto'].'">'. $row['nombre'] .'</a>';
-         echo '</td>';
-         echo '<td class = "two_objects_column">';
-         echo '<form method = "POST" action = "aprobar_proyectos.php">';
-         echo '<input type="hidden" name="id" value="'.$row['id_proyecto'].'" />';
-         echo '<input value="Aprobar" type="submit" class = "btn" name="aprobar"/>';
-         echo '<input value="Rechazar" type="submit" class = "btn" name="rechazar"/>';
-         echo '</form>';
-         echo '</td>';
-         echo '</tr>';
-      }
-      Database::disconnect();
-   ?>
-   </table>
+   <p class="label">En caso de rechazar un proyecto, este se eliminará. Para pedir cambios al proyecto antes de
+      aprobarlos por favor contacte a los estudiantes directamente.</p>
+   <div class="container">
+      <table class="general">
+         <?php
+         $pdo = Database::connect();
+         $sql = 'SELECT *  FROM r_proyectos WHERE id_docente = ? AND estatus = 0';
+         $q = $pdo->prepare($sql);
+         $q->execute(array($id_docente));
+         $proyectos = $q->fetchAll(PDO::FETCH_ASSOC);
+         foreach ($proyectos as $row) {
+            echo '<tr>';
+            echo '<td>';
+            echo '<a class="link" href="proyecto.php?id=' . $row['id_proyecto'] . '">' . $row['nombre'] . '</a>';
+            echo '</td>';
+            echo '<td class = "two_objects_column">';
+            echo '<form method = "POST" action = "aprobar_proyectos.php">';
+            echo '<input type="hidden" name="id" value="' . $row['id_proyecto'] . '" />';
+            echo '<input value="Aprobar" type="submit" class = "btn" name="aprobar"/>';
+            echo '<input value="Rechazar" type="submit" class = "btn" name="rechazar"/>';
+            echo '</form>';
+            echo '</td>';
+            echo '</tr>';
+         }
+         Database::disconnect();
+         ?>
+      </table>
    </div>
 
    <h1 class="label">Aprobados</h1>
-   <div class = "container">
-      <table class="general">  
-      <?php
-      $pdo = Database::connect();
-      $sql = 'SELECT *  FROM r_proyectos WHERE id_docente = ? AND estatus = 1';
-      $q = $pdo->prepare($sql);
-      $q->execute(array($id_docente));
-      $proyectos = $q->fetchAll(PDO::FETCH_ASSOC);
-      foreach ($proyectos as $row) {
-         echo '<tr>';
-         echo '<td>';
-         echo '<a class="link" href="proyecto.php?id='.$row['id_proyecto'].'">'. $row['nombre'] .'</a>';
-         echo '</td>';
-         echo '</tr>';
-      }
-      Database::disconnect();
-   ?>
+   <div class="container">
+      <table class="general">
+         <?php
+         $pdo = Database::connect();
+         $sql = 'SELECT *  FROM r_proyectos WHERE id_docente = ? AND estatus = 1';
+         $q = $pdo->prepare($sql);
+         $q->execute(array($id_docente));
+         $proyectos = $q->fetchAll(PDO::FETCH_ASSOC);
+         foreach ($proyectos as $row) {
+            echo '<tr>';
+            echo '<td>';
+            echo '<a class="link" href="proyecto.php?id=' . $row['id_proyecto'] . '">' . $row['nombre'] . '</a>';
+            echo '</td>';
+            echo '</tr>';
+         }
+         Database::disconnect();
+         ?>
       </table>
    </div>
 </body>
+
 </html>
--- crear_proyecto.php
+++ crear_proyecto.php
@@ -1,225 +1,236 @@
 <?php
-   session_start();
-	require 'database.php';
+session_start();
+require 'database.php';
+if ($_SESSION['matricula'] != null)
+   $matricula = $_SESSION['matricula'];
+else
+   header("Location:informativa.php");
 
-		$Error = null;
+$Error = null;
 
-	if ( !empty($_POST)) {
+if (!empty($_POST)) {
 
-		$name = $_POST['name'];
-		$poster = $_POST['poster'];
-		$video   = $_POST['video'];
-		$uf = $_POST['uf'];
-		$docente = $_POST['docente'];
-		$categoria = $_POST['categoria'];
-		$descripcion = $_POST['descripcion'];
-		
+   $name = $_POST['name'];
+   $poster = $_POST['poster'];
+   $video = $_POST['video'];
+   $uf = $_POST['uf'];
+   $docente = $_POST['docente'];
+   $categoria = $_POST['categoria'];
+   $descripcion = $_POST['descripcion'];
 
-		// validate input
-		$valid = true;
 
-		if (empty($name)) {
-			$Error = 'Por favor escribe un nombre';
-			$valid = false;
-		}
-		if (empty($poster)) {
-			$Error = 'Por favor proporcione un link al poster';
-			$valid = false;
-		}
-		if (empty($video)) {
-			$Error = 'Por favor proporcione un link al video';
-			$valid = false;
-		}
-		if (empty($uf)) {
-			$Error = 'Por favor seleccione una UF';
-			$valid = false;
-		}
-		if (empty($docente)) {
-			$Error = 'Por favor seleccione un docente';
-			$valid = false;
-		}
-		if (empty($categoria)) {
-			$Error = 'Por favor seleccione una categoria';
-			$valid = false;
-		}
-      if (empty($descripcion)) {
-			$Error = 'Por favor proporcione una descripcion';
-			$valid = false;
-		}
+   // validate input
+   $valid = true;
 
-		// insert data
-		if ($valid) {
-			$pdo = Database::connect();
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "INSERT INTO r_proyectos (id_proyecto, poster, description, estatus, nombre, video, 
+   if (empty($name)) {
+      $Error = 'Por favor escribe un nombre';
+      $valid = false;
+   }
+   if (empty($poster)) {
+      $Error = 'Por favor proporcione un link al poster';
+      $valid = false;
+   }
+   if (empty($video)) {
+      $Error = 'Por favor proporcione un link al video';
+      $valid = false;
+   }
+   if (empty($uf)) {
+      $Error = 'Por favor seleccione una UF';
+      $valid = false;
+   }
+   if (empty($docente)) {
+      $Error = 'Por favor seleccione un docente';
+      $valid = false;
+   }
+   if (empty($categoria)) {
+      $Error = 'Por favor seleccione una categoria';
+      $valid = false;
+   }
+   if (empty($descripcion)) {
+      $Error = 'Por favor proporcione una descripcion';
+      $valid = false;
+   }
+
+   // insert data
+   if ($valid) {
+      $pdo = Database::connect();
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "INSERT INTO r_proyectos (id_proyecto, poster, description, estatus, nombre, video, 
                                 fecha_registro, id_uf,  id_docente, id_edicion, id_categoria) 
                                 values (NULL, ?, ?, false, ?,?, current_timestamp(), ?, ?, 1, ?)";
-                                
-			$q = $pdo->prepare($sql);
-			$q->execute(array($poster,$descripcion, $name, $video, $uf, $docente, $categoria, ));
-			Database::disconnect();
 
-         $sql = 'SELECT * FROM r_proyectos WHERE nombre = ?';
-         $q = $pdo->prepare($sql);
-         $q->execute(array($name));
-         $row = $q->fetch(PDO::FETCH_ASSOC);
-         $id_proyecto = $row['id_proyecto'];
-			$sql = "INSERT INTO r_proyecto_estudiantes (id_registro, matricula, id_proyecto) 
+      $q = $pdo->prepare($sql);
+      $q->execute(array($poster, $descripcion, $name, $video, $uf, $docente, $categoria, ));
+      Database::disconnect();
+
+      $sql = 'SELECT * FROM r_proyectos WHERE nombre = ?';
+      $q = $pdo->prepare($sql);
+      $q->execute(array($name));
+      $row = $q->fetch(PDO::FETCH_ASSOC);
+      $id_proyecto = $row['id_proyecto'];
+      $sql = "INSERT INTO r_proyecto_estudiantes (id_registro, matricula, id_proyecto) 
                                 values (?, ?, ?)";
-                                
-			$q = $pdo->prepare($sql);
-			$q->execute(array(null, $_SESSION['estudiante'], $id_proyecto));
-			Database::disconnect();
-			header("Location: informativa.php");
-		}
-	}
+
+      $q = $pdo->prepare($sql);
+      $q->execute(array(null, $_SESSION['estudiante'], $id_proyecto));
+      Database::disconnect();
+      header("Location: informativa.php");
+   }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
-   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Crear Proyecto</title>
-      <link rel="stylesheet" href="CSS/style.css">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script>
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Crear Proyecto</title>
+   <link rel="stylesheet" href="CSS/style.css">
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+   <script>
       $(function () {
-        $("#header").load(<?php 
-        if($_SESSION['estudiante'] != null){
+         $("#header").load(<?php
+         if ($_SESSION['estudiante'] != null) {
             echo '"header_estudiante.php"';
-        } else if($_SESSION['admin'] != null){
+         } else if ($_SESSION['admin'] != null) {
             echo '"header_admin.php"';
-        } else if($_SESSION['juez'] != null){
+         } else if ($_SESSION['juez'] != null) {
             echo '"header_juez.php"';
-        } else if($_SESSION['docente'] != null){
+         } else if ($_SESSION['docente'] != null) {
             echo '"header_docente.php"';
-        }
-        ?>);
+         }
+         ?>);
       });
-    </script>
-  </head>
-  <body>
-    <div id="header"></div>
-      <div class = "ventana">
-         <h1 class="label"></h1>
-         <div class="container">
-      <form class = "login" action="crear_proyecto.php" method=POST>
-      <table>
-         
-         <tr>
-            <td><label for="name">Nombre del Proyecto:</label></td>
-            <td><input type="text" id="title" name="name" value="<?php echo !empty($name)?$name:'';?>"></td>
-            <td><input value="Agregar compañeros" type="submit" class = "btn" ></td> 
-         </tr>
+   </script>
+</head>
 
-         <tr>
-            <td><label for="poster">Link al poster:</label></td>
-            <td><input type="text" id="linkp" name="poster" value="<?php echo !empty($poster)?$poster:'';?>"></td>
-         </tr>
-         <tr>
-            <td><label for="video">Link al video:</label></td>
-            <td><input type="text" id="linkv" name="video" value="<?php echo !empty($video)?$video:'';?>"></td>
-            </tr>
+<body>
+   <div id="header"></div>
+   <div class="ventana">
+      <h1 class="label"></h1>
+      <div class="container">
+         <form class="login" action="crear_proyecto.php" method=POST>
+            <table>
 
-         <tr>
-            <td><label for="link">Seleccionar UF:</label></td>
-            <td>     
-            <select name = "uf">
-               <?php
-                  $pdo = Database::connect();
-                  $query = 'SELECT * FROM r_ufs';
-                  foreach ($pdo->query($query) as $row) {
-                     echo "<option value='" . $row['id_uf'] . "'>" . $row['nombre'] . "</option>";
-                  }
-                  Database::disconnect();
-               ?>
-            </select>
-            </td>
-            </tr>
+               <tr>
+                  <td><label for="name">Nombre del Proyecto:</label></td>
+                  <td><input type="text" id="title" name="name" value="<?php echo !empty($name) ? $name : ''; ?>"></td>
+                  <td><input value="Agregar compañeros" type="submit" class="btn"></td>
+               </tr>
 
-            <tr>
-            <td><label for="docente">Seleccionar docente:</label></td>
-            <td>    
-            <select name = "docente">
-               <?php
-                  $pdo = Database::connect();
-                  $query = 'SELECT * FROM r_docentes';
-                  foreach ($pdo->query($query) as $row) {
-                     echo "<option value='" . $row['id_docente'] . "'>" . $row['nombre'] . " " . $row['apellidoP'] . "</option>";
-                  }
-                  Database::disconnect();
-               ?>
-            </select>
-            </td>
-            </tr>
+               <tr>
+                  <td><label for="poster">Link al poster:</label></td>
+                  <td><input type="text" id="linkp" name="poster" value="<?php echo !empty($poster) ? $poster : ''; ?>">
+                  </td>
+               </tr>
+               <tr>
+                  <td><label for="video">Link al video:</label></td>
+                  <td><input type="text" id="linkv" name="video" value="<?php echo !empty($video) ? $video : ''; ?>">
+                  </td>
+               </tr>
 
-            <tr>
-            <td><label for="categoria">Seleccionar categoría:</label></td>
-            <td>    
-            <select name = "categoria">
-               <?php
-                  $pdo = Database::connect();
-                  $query = 'SELECT * FROM r_categorias';
-                  foreach ($pdo->query($query) as $row) {
-                     echo "<option value='" . $row['id_categoria'] . "'>" . $row['nombre'] . "</option>";
-                  }
-                  Database::disconnect();
-               ?>
-            </select>
-            </td>
-            </tr>
+               <tr>
+                  <td><label for="link">Seleccionar UF:</label></td>
+                  <td>
+                     <select name="uf">
+                        <?php
+                        $pdo = Database::connect();
+                        $query = 'SELECT * FROM r_ufs';
+                        foreach ($pdo->query($query) as $row) {
+                           echo "<option value='" . $row['id_uf'] . "'>" . $row['nombre'] . "</option>";
+                        }
+                        Database::disconnect();
+                        ?>
+                     </select>
+                  </td>
+               </tr>
 
-            <tr>
-            <td><label for="descripcion">Descripcion:</label></td>
-            <td><textarea id="comment" name="descripcion"><?php echo !empty($descripcion)?$descripcion:'';?></textarea></td>
-            </tr>
+               <tr>
+                  <td><label for="docente">Seleccionar docente:</label></td>
+                  <td>
+                     <select name="docente">
+                        <?php
+                        $pdo = Database::connect();
+                        $query = 'SELECT * FROM r_docentes';
+                        foreach ($pdo->query($query) as $row) {
+                           echo "<option value='" . $row['id_docente'] . "'>" . $row['nombre'] . " " . $row['apellidoP'] . "</option>";
+                        }
+                        Database::disconnect();
+                        ?>
+                     </select>
+                  </td>
+               </tr>
 
-         </table>
-         <div>
-            <button class="cancel">Cancelar</button>
-            <input value="Registrar Proyecto" type="submit" class = "btn" id="convert-btn">
-         </div>
-         <?php if (($Error != null)) ?>
-				<div class="Error"><?php echo $Error;?></div>
-      </form>
-   </body>
+               <tr>
+                  <td><label for="categoria">Seleccionar categoría:</label></td>
+                  <td>
+                     <select name="categoria">
+                        <?php
+                        $pdo = Database::connect();
+                        $query = 'SELECT * FROM r_categorias';
+                        foreach ($pdo->query($query) as $row) {
+                           echo "<option value='" . $row['id_categoria'] . "'>" . $row['nombre'] . "</option>";
+                        }
+                        Database::disconnect();
+                        ?>
+                     </select>
+                  </td>
+               </tr>
+
+               <tr>
+                  <td><label for="descripcion">Descripcion:</label></td>
+                  <td><textarea id="comment"
+                        name="descripcion"><?php echo !empty($descripcion) ? $descripcion : ''; ?></textarea></td>
+               </tr>
+
+            </table>
+            <div>
+               <button class="cancel">Cancelar</button>
+               <input value="Registrar Proyecto" type="submit" class="btn" id="convert-btn">
+            </div>
+            <?php if (($Error != null)) ?>
+            <div class="Error">
+               <?php echo $Error; ?>
+            </div>
+         </form>
+</body>
+
 </html>
- 
-<script>
-   ( function( $ ) {
-  
-  var url = $( '#linkv' ),
-      btn = $( '#convert-btn' );
 
-  btn.on( 'click', function( event ) {
-    found = url.val().match( /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/ );
-    
-    if ( found[1].length ) {
-      new_url = 'https://www.youtube.com/embed/' + found[1];
-      
-      url.val( new_url );
-    }
-  } );
-  
-} )( jQuery );
+<script>
+   (function ($) {
+
+      var url = $('#linkv'),
+         btn = $('#convert-btn');
+
+      btn.on('click', function (event) {
+         found = url.val().match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+
+         if (found[1].length) {
+            new_url = 'https://www.youtube.com/embed/' + found[1];
+
+            url.val(new_url);
+         }
+      });
+
+   })(jQuery);
 </script>
 
 <script>
-   ( function( $ ) {
-  
-  var url = $( '#linkp' ),
-      btn = $( '#convert-btn' );
+   (function ($) {
 
-  btn.on( 'click', function( event ) {
-    found = url.val().match( /d\/([A-Za-z0-9_\-]+)/ );
-    if ( found[1].length ) {
-      new_url = 'https://drive.google.com/uc?export=view&id=' + found[1];
-      url.val( new_url );
-    }
-  } );
-  
-} )( jQuery );
+      var url = $('#linkp'),
+         btn = $('#convert-btn');
+
+      btn.on('click', function (event) {
+         found = url.val().match(/d\/([A-Za-z0-9_\-]+)/);
+         if (found[1].length) {
+            new_url = 'https://drive.google.com/uc?export=view&id=' + found[1];
+            url.val(new_url);
+         }
+      });
+
+   })(jQuery);
 </script>
-
