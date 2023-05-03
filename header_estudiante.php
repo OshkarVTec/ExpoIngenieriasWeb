72baf0688cdcac3552ef7dc646e1201bcf2f56d1
OshkarVTec <a01275287@tec.mx>
2023-05-03 17:04:28 -0600

    Añadí anuncios a header de estudiantes

--- header_estudiante.php
+++ header_estudiante.php
@@ -21,6 +21,7 @@ $proyecto = $p_estudiantes['id_proyecto'];
        <li><?php echo '<a href="cuenta.php?id_usuario='.$_SESSION['id_usuario'].'">Mi Cuenta</a>';?></li>
        <li><a href="mis_proyectos.php">Mis Proyectos</a></li>
        <li><a href="rubrica_evaluacion.php">Criterio de evaluación</a></li>
+       <li><a href="informativa.php">Anuncios</a></li>
       </ul>
       </div>
    </nav>
