7ba63438771918fb50a42826cfdc4147c77076a6
OshkarVTec <a01275287@tec.mx>
2023-05-03 09:34:59 -0600

    Headers añadidos

--- header_juez.php
+++ header_juez.php
@@ -1,17 +1,19 @@
-<?php 
-  session_start();
+<?php
+session_start();
 ?>
 <header class="header">
-    <div class="logo">
-      <img src="IMG/logo-expo.png" alt="Logo de la pagina">
+  <div class="logo">
+    <img src="IMG/logo-expo.png" alt="Logo de la pagina">
+  </div>
+  <nav class="menu">
+    <ul class="nav-links">
+      <li><a href="evaluar.php">Evaluar</a></li>
+      <li><a href="ver_proyectos.php">Ver proyectos</a></li>
+      <li><a href="informativa_juez.html">Anuncios</a></li>
+      <li>
+        <?php echo '<a href="cuenta.php?id_usuario=' . $_SESSION['id_usuario'] . '">Mi Cuenta</a>'; ?>
+      </li>
+    </ul>
     </div>
-    <nav class="menu">
-       <ul class="nav-links">  
-       <li><a href="evaluar.php">Evaluar</a></li>
-       <li><a href="ver_proyectos.php">Ver proyectos</a></li>
-       <li><a href="informativa_juez.html">Anuncios</a></li>
-       <li><?php echo '<a href="cuenta.php?id_usuario='.$_SESSION['id_usuario'].'">Mi Cuenta</a>';?></li>
-      </ul>
-      </div>
-   </nav>
-  </header>
+  </nav>
+</header>
