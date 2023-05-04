30dfd6d2323a4aaa6963b9709a5b2c13822f709a
RaymundoDiazs <73440290+RaymundoDiazs@users.noreply.github.com>
2023-05-03 18:36:01 -0600

    Add files via upload

--- informativa.php
+++ informativa.php
@@ -1,6 +1,4 @@
-<?php
-    session_start();
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,20 +9,20 @@
     <title>Informativa</title>
     <link rel="stylesheet" href="CSS/style.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-   <script> 
-    $(function(){
-      $("#header").load(<?php 
-        if($_SESSION['matricula'] != null){
-            echo '"header_estudiante.php"';
-        } else if($_SESSION['admin'] != null){
-            echo '"header_admin.php"';
-        } else if($_SESSION['juez'] != null){
-            echo '"header_juez.php"';
-        } else if($_SESSION['docente'] != null){
-            echo '"header_docente.php"';
-        }
-        ?>); 
-    });
+    <script> 
+      $(function(){
+        $("#header").load(<?php 
+          if($_SESSION['estudiante'] != null){
+              echo '"header_estudiante.php"';
+          } else if($_SESSION['admin'] != null){
+              echo '"header_admin.php"';
+          } else if($_SESSION['juez'] != null){
+              echo '"header_juez.php"';
+          } else if($_SESSION['docente'] != null){
+              echo '"header_docente.php"';
+          }
+          ?>); 
+      });
     </script> 
   </head>
   <body>
@@ -33,11 +31,32 @@
     <section id="container-informativa">
       <div id="container-seccion-izquierda">
         <h1>Inicia Expoingenierias</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Iusto incidunt doloribus quia repudiandae error, aliquid aspernatur 
-          labore illo veniam inventore velit voluptates ipsa accusantium nulla 
-          voluptatum corrupti fugiat ratione? Molestiae.</p>
+        
+        <?php  
+require 'database.php';
+$pdo = Database::connect();
+$sql = "SELECT * FROM r_anuncios WHERE vigente = 1";
+$stmt = $pdo->query($sql);
+$anuncios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($anuncios as $anuncio) {
+  echo '<div class="mySlides fade">';
+  echo '<p>' . $anuncio['contenido'] . '</p>';
+  echo '</div>';
+}
+?>
+
+<?php
+$n = 4;
+for ($i = 1; $i <= $n; $i++) {
+  echo '<span class="dot" onclick="currentSlide(' . $i . ')"></span>';
+}
+?>
+  
+
       </div>
+
+
       <div id="container-seccion-derecha" class="slideshow-container">
         <div id="container-seccion-derecha-img">
           <div class="mySlides fade">
@@ -55,9 +74,9 @@
 
         <!-- The dots/circles -->
         <!-- <div id="container-seccion-derecha-btnes" style="text-align: center"> -->
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(10)"></span>
+        <span class="dot" onclick="currentSlide(11)"></span>
+        <span class="dot" onclick="currentSlide(12)"></span>
         <!-- </div> -->
       </div>
     </section>
