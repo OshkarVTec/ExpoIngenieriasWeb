fe7466bf0f8e488fd0bf18a2ca0249c5ea22917a
OshkarVTec <a01275287@tec.mx>
2023-05-04 20:36:11 -0600

    Muestra los aprobados

--- ver_proyectos.php
+++ ver_proyectos.php
@@ -48,23 +48,35 @@ if (!($_SESSION['juez'] != null || $_SESSION['admin'] != null || $_SESSION['doce
    $sql = 'SELECT * FROM r_niveles_desarrollo ORDER BY id_nivel';
    foreach ($pdo->query($sql) as $row) {
       echo '<h1 class="label">Nivel de desarrollo: ' . $row['nombre'] . '</h1>';
+      $sql = 'SELECT COUNT(id_proyecto) FROM r_proyectos WHERE id_nivel = ? AND id_edicion = ? AND estatus = true';
+      $q = $pdo->prepare($sql);
+      $q->execute(array($row['id_nivel'], $id_edicion));
+      $aprobados = $q->fetchColumn();
+      $sql = 'SELECT COUNT(id_proyecto) FROM r_proyectos WHERE id_nivel = ? AND id_edicion = ?';
+      $q = $pdo->prepare($sql);
+      $q->execute(array($row['id_nivel'], $id_edicion));
+      $total = $q->fetchColumn();
+      echo '<h1 class="label">Número de aprobados: ' . $aprobados . '/' . $total . '</h1>';
       echo '<div class = "container">';
       echo '<table class="general">';
-      $sql = 'SELECT * FROM r_proyectos WHERE id_nivel = ? AND id_edicion = ?';
+      $sql = 'SELECT * FROM r_proyectos WHERE id_nivel = ? AND id_edicion = ? ORDER BY estatus DESC';
       $q = $pdo->prepare($sql);
       $q->execute(array($row['id_nivel'], $id_edicion));
       $proyectos = $q->fetchAll(PDO::FETCH_ASSOC);
       foreach ($proyectos as $row) {
          echo '<tr>';
-         echo '<td>';
+         echo '<td class="row-nombre-proyecto">';
          echo '<a class="link" href="proyecto.php?id_proyecto=' . $row['id_proyecto'] . '">' . $row['nombre'] . '</a>';
+         echo '</td>';
+         echo '<td>';
+         echo '<p>' . (($row['estatus'] == true) ? 'Aprobado' : 'No aprobado') . '</p>';
          echo '</td>';
          $sql = 'SELECT * FROM r_categorias WHERE id_categoria = ?';
          $q = $pdo->prepare($sql);
          $q->execute(array($row['id_categoria']));
          $categoria = $q->fetch(PDO::FETCH_ASSOC);
          echo '<td>';
-         echo '<p>' .$categoria['nombre']. '</p>';
+         echo '<p>' . $categoria['nombre'] . '</p>';
          echo '</td>';
          echo '</tr>';
       }
